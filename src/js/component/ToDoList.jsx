4a851e643a06c74c9b7bb6701043339613b2b79d
Sofia <sofiaxrk@gmail.com>
2022-02-06 14:04:03 +0000

    guardando 2

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const ToDoList = () => {
+	const [list, setList] = useState([]);
+	const [tarea, setTarea] = useState('');
+
+	const Submit = (e) => {
+		e.preventDefault();
+		setList([...list, newTask(tarea, false)]);
+	};
+
+	useEffect(() => {
+		fetch('https://assets.breatheco.de/apis/fake/todos/'), {
+			method: 'GET',
+			headers: {
+				'Content-type': "application/json"
+			}
+		}).then((response) => {
+			response.json();
+		}).then((res) => setList(res))
+		.catch((err) => console.error(err));
+	}, []);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<form onSubmit={Submit}>
+			<input onChange=((e) => setTarea type="text" />
+		</form>
+		</>
 	);
 };
 
-export default Home;
+export default ToDoList;
 
 //las tasks se agregan cuando el usuario apreta enter
 //delete icon shows when task is hover
